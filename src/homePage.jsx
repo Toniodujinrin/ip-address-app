6c0b3d719a2930692334fa334f700f509ef18688
Toniodujinrin <todujinrin@gmail.com>
2022-09-08 16:21:57 +0100

    first commit

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
 import Info from "./components/ipInfo";
 import Search from "./components/searchBar";
-i
+import axios from "axios";
+
 class Home extends Component {
   state = {
     ip: "",
+    data: {},
   };
 
-  async componentDidMount(){
-    const{data}
+  async componentDidUpdate(pP, pS, sS) {
+    const { data } = await axios.get(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_atr3fj0XwgmeyPehNlOCmFFQ1GG6o&ipAddress=${this.state.ip}`
+    );
+    if (this.state.data !== pS.data) {
+      this.setState({ data });
+    } else console.log(data);
   }
 
-  handleSearch = () => {};
+  handleSearch = (value) => {
+    this.setState({ ip: value });
+  };
   render() {
     return (
       <div id="main">
         <h1 id="title">IP Address Tracker</h1>
         <Search handleSearch={this.handleSearch} />
-        <Info />
+        <Info data={this.state.data} />
       </div>
     );
   }
